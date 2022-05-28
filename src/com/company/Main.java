1fdae22e64c7f7e286bc5e4a2c7c0f7cb63474a6
Samuel <slemm@stu.vistula.edu.pl>
2022-05-28 12:54:36 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.UUID;
+import java.util.Scanner;
+
+
+public class Main {
+            public static void main(String[] args) throws IOException {
+                NameList.Start();
+            }
+        }
+    }
+}
